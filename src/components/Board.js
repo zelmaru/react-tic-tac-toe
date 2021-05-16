3dab33e4e7adf2dc9851476d053da2d10cc5bf88
zelmaru <zelenkovait@gmail.com>
2021-05-16 13:45:32 +0200

    declaring a winner + end game if no fields left

--- src/components/Board.js
+++ src/components/Board.js
@@ -6,6 +6,10 @@ const Board = () => {
 	const [xIsNext, setXIsNext] = useState(true);
 
 	const handleClick = (i) => {
+		if (calculateWinner(squares) || squares[i] || !squares.includes(null)) {
+			// !squares.includes(null) - I added this to not proceed the game if all squares are used and there is no winner
+			return; // ignore a click if someone has won the game or if a Square is already filled
+		}
 		const newSquares = squares.slice(); // make a shallow copy of the array
 		newSquares[i] = xIsNext ? "X" : "O";
 		setSquares(newSquares);
@@ -16,7 +20,40 @@ const Board = () => {
 		return <Square value={squares[i]} onClick={() => handleClick(i)} />;
 	};
 
-	const status = `Next player: ${xIsNext ? "X" : "O"}`;
+	const calculateWinner = () => {
+		const lines = [
+			[0, 1, 2],
+			[3, 4, 5],
+			[6, 7, 8],
+			[0, 3, 6],
+			[1, 4, 7],
+			[2, 5, 8],
+			[0, 4, 8],
+			[2, 4, 6],
+		];
+
+		for (let i = 0; i < lines.length; i++) {
+			const [a, b, c] = lines[i];
+			if (
+				squares[a] &&
+				squares[a] === squares[b] &&
+				squares[a] === squares[c]
+			) {
+				return squares[a];
+			}
+		}
+		return null;
+	};
+
+	const winner = calculateWinner(squares);
+	let status;
+	if (winner) {
+		status = `Winner: ${winner}`;
+	} else if (!squares.includes(null)) {
+		status = "Game over, there is no winner";
+	} else {
+		status = `Next player: ${xIsNext ? "X" : "O"}`;
+	}
 
 	return (
 		<>
