fa4bcdb2b70a1a8e529fbba5b56849a7d6e850df
zelmaru <zelenkovait@gmail.com>
2021-05-16 11:59:49 +0200

    starter code

---
+++ src/components/Board.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Square from "./Square";
+
+const Board = () => {
+	const renderSquare = (i) => {
+		return <Square />;
+	};
+
+	const status = "Next player: X";
+
+	return (
+		<>
+			<div className="status">{status}</div>
+			<div className="board-row">
+				{renderSquare(0)}
+				{renderSquare(1)}
+				{renderSquare(2)}
+			</div>
+			<div className="board-row">
+				{renderSquare(3)}
+				{renderSquare(4)}
+				{renderSquare(5)}
+			</div>
+			<div className="board-row">
+				{renderSquare(6)}
+				{renderSquare(7)}
+				{renderSquare(8)}
+			</div>
+		</>
+	);
+};
+
+export default Board;
---
+++ src/components/Square.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Square = () => {
+	return <button className="square"></button>;
+};
+
+export default Square;
